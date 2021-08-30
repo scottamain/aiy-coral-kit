5bfc3b294f805b3a319263c795820e7e72723f21
Scott Main <scottamain@gmail.com>
2021-08-30 20:58:33 +0100

    rename demo script, add display check and remove pose demo

--- run_demo.sh
+++ run_demo.sh
@@ -5,8 +5,11 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 bash ${SCRIPT_DIR}/install_requirements.sh
 
+if [[ ! $DISPLAY ]]; then
+  echo "ERROR: No display detected. This demo requires a desktop display."
+  exit;
+fi
 
-# TODO: First check if display is available
 while true; do
   echo
   read -p "Run a quick hardware test? (y/n) " yn
@@ -28,17 +31,6 @@ while true; do
   case $yn in
     [Yy]* )
       (cd "${SCRIPT_DIR}" && python3 "detect_faces.py"); break;;
-    [Nn]* ) break;;
-    * ) echo "Please answer yes or no.";;
-  esac
-done
-
-while true; do
-  echo
-  read -p "Try the pose detection demo? (y/n) " yn
-  case $yn in
-    [Yy]* )
-      (cd "${SCRIPT_DIR}" && python3 "detect_pose.py"); break;;
     [Nn]* ) break;;
     * ) echo "Please answer yes or no.";;
   esac
